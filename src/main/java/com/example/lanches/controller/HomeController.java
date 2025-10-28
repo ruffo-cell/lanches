b084d96372d188f68650dc30769d9759ebb36377
ruffo-cell <ruffofla@gmail.com>
2025-10-28 11:48:50 -0300

    Initial commit

---
+++ src/main/java/com/example/lanches/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.lanches.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "home";
+    }
+
+    @GetMapping("/cardapio")
+    public String cardapio(Model model) {
+        return "cardapio";
+    }
+}
+
